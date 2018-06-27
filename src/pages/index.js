1d3af5138901521d738b6bb056d52bfc8765e0c6
Ridge Frederick <ridge.frederick@gmail.com>
2018-06-26 21:47:22 -0500

    Switch to gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>hello.</h1>
+    <p>there's nothing here right now.</p>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/posts/_drafts/2018-06-26--hello-world.md
@@ -0,0 +1,6 @@
+---
+path: "/posts/hello-world"
+date: "2018-06-26"
+title: "Hello World"
+---
+Nothing much here. Just following protocol for the beginning of something new.
